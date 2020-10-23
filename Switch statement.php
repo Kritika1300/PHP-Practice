d69b52817294c23dc8910d777a8d1e99d7ea82d4
Kritika <kaur.kritika.kritika@gmail.com>
2020-10-23 18:18:28 +0530

    Switch statement

---
+++ Switch statement.php
@@ -0,0 +1,41 @@
+<?php
+echo "Enter an alphabet : ";
+$alphabet = "h";
+echo $alphabet;
+echo "<br>";
+switch($alphabet){
+    case 'a' :
+     echo "Vowel";
+     break;
+    case 'i' :
+     echo "Vowel";
+    break;
+    case 'o' :
+     echo "Vowel";
+    break;
+    case 'e' :
+     echo "Vowel";
+    break;
+    case 'u' :
+     echo "Vowel";
+    break;
+    case 'A' :
+     echo "Vowel";
+    break;
+    case 'E' :
+     echo "Vowel";
+    break;
+    case 'I' :
+     echo "Vowel";
+    break;
+    case 'O' :
+     echo "Vowel";
+    break;
+    case 'U' :
+     echo "Vowel";
+    break;
+    default :
+     echo "Consonant";
+}
+
+?>
