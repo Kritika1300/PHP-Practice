cd8fea4702f81c27be79b3bd503de52484be9bd4
Kritika <kaur.kritika.kritika@gmail.com>
2020-10-31 18:21:45 +0530

    To check whether a no. is +ve/-ve/0

---
+++ To check whether a number is +ve,-ve or 0.php
@@ -0,0 +1,14 @@
+<?php
+echo "Given number :";
+$num = 2;
+echo " $num <br>";
+if($num > 0){
+echo "The number is POSITIVE";
+}
+else if($num < 0){
+echo "The number is NEGATIVE";
+}
+else{
+echo "The number is ZERO"; 
+}
+?>
